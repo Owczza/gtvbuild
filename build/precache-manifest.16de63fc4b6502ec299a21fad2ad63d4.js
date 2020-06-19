551a1b93f81fe6fb5f5861f8e5f8b75f05f7aa0c
Aleksandra Moss <37577436+Owczza@users.noreply.github.com>
2020-06-19 20:57:29 +0200

    Add files via upload

---
+++ build/precache-manifest.16de63fc4b6502ec299a21fad2ad63d4.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "4b740863c2cba784f64e87212edd130a",
+    "url": "/index.html"
+  },
+  {
+    "revision": "3690ec9fbffc5879bb66",
+    "url": "/static/css/main.ca29437a.chunk.css"
+  },
+  {
+    "revision": "241a329c80cfc3ef97e3",
+    "url": "/static/js/2.9ee70ffe.chunk.js"
+  },
+  {
+    "revision": "507c4436aa55e48136618e82a79cbf78",
+    "url": "/static/js/2.9ee70ffe.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "3690ec9fbffc5879bb66",
+    "url": "/static/js/main.716c3002.chunk.js"
+  },
+  {
+    "revision": "d42654052f59ad130199",
+    "url": "/static/js/runtime-main.4e922f4c.js"
+  }
+]);
